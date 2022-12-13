877f6b5e79d9cf66f2a8e00892bc575666d98086
Alena Gladkova <alenagladkova@Alenas-Air.lan>
2022-12-12 16:19:23 -0800

    wish me luck

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 
 window.addEventListener("load", function () {
 
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+        event.preventDefault();
+    });
+
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoLevel = document.querySelector("input[name=cargoMass]");
+
+
+    let list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
 
     let listedPlanets;
     let listedPlanetsResponse = myFetch();
@@ -11,22 +25,7 @@ window.addEventListener("load", function () {
     }).then(function () {
         console.log(listedPlanets);
 
-        let pilot = document.querySelector("input[name=pilotName]");
-        let copilot = document.querySelector("input[name=copilotName]");
-        let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoLevel = document.querySelector("input[name=cargoMass]");
-
-
-        let list = document.getElementById("faultyItems");
-        list.style.visibility = "hidden";
-
-        let form = document.querySelector("form");
-        form.addEventListener("submit", function (event) {
-            formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
-            event.preventDefault();
-        });
         let destination = pickPlanet(listedPlanets);
-
         addDestinationInfo(document,
             listedPlanets[destination].name,
             listedPlanets[destination].diameter,
